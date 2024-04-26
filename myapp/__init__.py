a3616845e3c03f69113d1f53b7f87e09deeb1e3f
Kriegler lawoko <Krieglerlawokojudge@gmail.com>
2024-04-26 09:39:57 -0700

    new

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask
+
+m flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from utils import generate_token
 
 # Define Flask extensions
 db = SQLAlchemy()
@@ -8,14 +10,19 @@ login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__, template_folder='custom_templates')
+    
+    # Load configuration from environment variables or use defaults
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = 'your_secret_key'  # Set a secret key for session management
-
+    app.config['SECRET_KEY'] = generate_token()  # Set a secret key for session management
+    
+    # Configure Flask-Login
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'  # Specify the view for login
+    
     # Initialize extensions
     db.init_app(app)
-    login_manager.init_app(app)
-
+    
     # Additional app setup goes here
-
+    
     return app
--- myapp/app.py
+++ myapp/app.py
@@ -21,4 +21,4 @@ login_manager.init_app(app)
 from routes import *
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
