55d18408b4ce90bf385251ef60c16c925aa63754
Kriegler lawoko <Krieglerlawokojudge@gmail.com>
2024-05-20 22:20:19 +0300

    tweak

--- myapp/pytest.py
+++
@@ -1,4 +0,0 @@
-from app import db
-from models import *
-
-user = User.query.filter_by(username=username).first()
--- myapp/run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-# Create the Flask app instance
-app = create_app('HarmonyHub')
-
-if __name__ == '__main__':
-    # Run the Flask development server
-    app.run(debug=True)
